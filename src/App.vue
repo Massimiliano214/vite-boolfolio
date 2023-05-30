8277ad4aa9b444fd1108d75c3101084be110db7b
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-05-30 16:25:16 +0200

    added main section

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 
   import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
 
   export default {
     components: {
-      AppHeader
+      AppHeader,
+      AppMain
     }
   }
 
@@ -12,6 +14,7 @@
 
 <template>
   <AppHeader/>
+  <AppMain></AppMain>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppMain.vue
@@ -0,0 +1,39 @@
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'AppMain',
+        data() {
+            return {
+                projects: [],
+                contentMaxLenght: 185,
+                baseUrl: 'http://127.0.0.1:8000',
+                currentPage: 1,
+                lastPage: null
+            }
+        },
+        methods: {
+            getProjects(goToPage) {
+                
+                axios.get(`${this.baseUrl}/api/projects`, {
+                    params: {
+                        page: goToPage
+                    }
+                }).then(response => {
+                    console.log(response);
+                })
+            }
+        },
+        mounted() {
+            this.getProjects(1);
+        }
+    }
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
