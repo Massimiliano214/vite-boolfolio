8a1c0c153bfd22fc329345afff6d3306dfeccc4e
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-05-30 17:08:08 +0200

    working cards with backup photo

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -14,7 +14,8 @@
 
 <template>
     <div class="card">
-        <img :src="image" class="card-img-top">
+        <img v-if="image != 'http://127.0.0.1:8000/storage/null'" :src="image" class="card-img-top">
+        <img v-else src="https://cdn.icon-icons.com/icons2/1462/PNG/512/120nophoto_100007.png" class="card-img-top"/>
         <div class="card-body">
             <h5 class="card-title">{{title}}</h5>
             <h5>{{type}}</h5>
@@ -25,5 +26,8 @@
 </template>
 
 <style lang="scss" scoped>
-
+    .card-img-top {
+        width: 60%;
+        margin: auto;
+    }
 </style>
